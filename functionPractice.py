9ed4fa6ca5af6d6988e12f2791e1cb875cf160b8
Yunf.li37@gmail.com <Yunf.li37@gmail.com>
2017-09-07 11:59:02 -0400

    more functions

--- functionPractice.py
+++ functionPractice.py
@@ -25,4 +25,15 @@ def say_Hello_with_name(name):
 say_Hello_with_name("Jamie")
 
 # if you call a function with an argument but doesn't put an arugment inside, it will cause an error
+# you can set a defualt for a parameter
+def say_Hello_safe(name, in_class ="yes"):
+	print("%s, %s is in the class" %(in_class, name));
 
+
+#function always return a value
+#return value is a function chance to send one and only one thing to whoever called it
+
+def return_user_name(name):
+	normalized_name = name.upper();
+	return normalized_name;
+print(return_user_name("Chris"));
